e1cf6c77a8aef611d401392b2e34673a00d28e9c
harsh <harshvsingh8@gmail.com>
2018-02-18 08:27:37 +0530

    cleaned up packages

--- packages/magit-popup-20180119.111/magit-popup-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; magit-popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
-;;;;;;  (23170 52658 477918 944000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; magit-popup-autoloads.el ends here
